e8e1b695623baeeb474b98c28dc72357dfbebaca
Dominic Galasso <DominicGalasso@gamestop.com>
2012-07-24 13:37:12 -0500

    Added HtmlResult which returns name, type and value

---
+++ ImpulseReSTCore/ActionResults/HtmlResult.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Serialization;
+
+namespace ImpulseReSTCore.ActionResults
+{
+
+    class HtmlResult : ActionResult
+    {
+        private readonly object _objectToSerialize;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HtmlResult"/> class.
+        /// </summary>
+        /// <param name="objectToSerialize">The object to serialize to Html.</param>
+        public HtmlResult(object objectToSerialize)
+        {
+            _objectToSerialize = objectToSerialize;
+        }
+
+        /// <summary>
+        /// Gets the object to be serialized to Html.
+        /// </summary>
+        public object ObjectToSerialize
+        {
+            get { return _objectToSerialize; }
+        }
+
+
+        /// <summary>
+        /// Serialises the object that was passed into the constructor to HTML and writes the corresponding HTML to the result stream.
+        /// </summary>
+        /// <param name="context">The controller context for the current request.</param>
+        public override void ExecuteResult(ControllerContext context)
+        {
+            HttpResponseBase response = context.HttpContext.Response;
+            response.ContentType = "text/html";
+
+            var viewData = new ViewDataDictionary();
+            var tempData = new TempDataDictionary();
+
+            string result = "";
+            using (StringWriter sw = new StringWriter())
+            {
+                ViewEngineResult viewResult = ViewEngines.Engines.FindView(context, "~/bin/RestViews/Html.cshtml", null);
+                ViewContext viewContext = new ViewContext(context, viewResult.View, viewData, tempData, sw);
+                viewResult.View.Render(viewContext, sw);
+
+                result = sw.GetStringBuilder().ToString();
+            }
+
+            response.Write(result);
+        }
+
+    }
+}
---
+++ ImpulseReSTCore/Models/HtmlModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Web.Mvc;
+
+
+namespace ImpulseReSTCore.Models
+{
+
+    public struct ObjectStruct
+    {
+        public string ObjectName;
+        public string ObjectValue;
+        public string ObjectType;
+    }
+    
+    public class HtmlModel
+    {
+        public ObjectStruct[] htmlObjects;
+        private string ModelName;
+
+        public HtmlModel(object objectToSerialize)
+        {
+            Populate(objectToSerialize);
+        }
+
+        public string GetModelName()
+        {
+            return ModelName;
+        }
+        private void Populate(object objectToSerialize)
+        {
+            if (objectToSerialize == null)
+                return;
+
+            PropertyInfo[] objectProperties = objectToSerialize.GetType().GetProperties();
+            ModelName = objectToSerialize.GetType().Name;
+
+            htmlObjects = new ObjectStruct[objectProperties.Length];
+
+            for (int i = 0; i < objectProperties.Length; ++i)
+            {
+                htmlObjects[i].ObjectName = objectProperties[i].Name;
+
+                object t = objectProperties[i].GetValue(objectToSerialize, null);
+                if (t != null)
+                {
+                    htmlObjects[i].ObjectValue = t.ToString();
+                }
+                else
+                {
+                    htmlObjects[i].ObjectValue = "NULL";
+                }
+
+                htmlObjects[i].ObjectType = objectProperties[i].PropertyType.FullName;
+            }
+
+        }
+    }
+
+}
