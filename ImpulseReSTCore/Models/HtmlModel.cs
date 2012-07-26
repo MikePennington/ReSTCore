c3f5ee0817f013c8cc3cbc51507bc7e9e3467de8
Dominic Galasso <DominicGalasso@gamestop.com>
2012-07-26 15:18:14 -0500

    Changed the html model to accept multiple objects

--- ImpulseReSTCore/Models/HtmlModel.cs
+++ ImpulseReSTCore/Models/HtmlModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -9,20 +10,27 @@ using System.Web.Mvc;
 namespace ImpulseReSTCore.Models
 {
 
+    public struct TypeStruct
+    {
+        public ObjectStruct[] htmlObjects;
+
+    }
+    
     public struct ObjectStruct
     {
-        public string ObjectName;
         public string ObjectValue;
         public string ObjectType;
     }
     
     public class HtmlModel
     {
-        public ObjectStruct[] htmlObjects;
+        public List<TypeStruct> typeObjects;
         private string ModelName;
+        public string[] htmlTitles;
 
         public HtmlModel(object objectToSerialize)
         {
+            typeObjects = new List<TypeStruct>();
             Populate(objectToSerialize);
         }
 
@@ -35,28 +43,69 @@ namespace ImpulseReSTCore.Models
             if (objectToSerialize == null)
                 return;
 
+           PropertyInfo countProperty = objectToSerialize.GetType().GetProperty("Count");
+            
+            if (countProperty != null)
+            {
+                    IEnumerable p = (IEnumerable) objectToSerialize;
+
+                    foreach (object objectIter in p)
+                    {
+                        if (htmlTitles == null)
+                        {
+                            GetHtmlTitles(objectIter);
+                        }
+
+                        GetHtmlObjects(objectIter);
+                    }
+            }
+            else
+            {
+                if (htmlTitles == null)
+                {
+                    GetHtmlTitles(objectToSerialize);
+                }
+
+                GetHtmlObjects(objectToSerialize);
+            }
+
+        }
+
+        private void GetHtmlObjects(object objectToSerialize)
+        {
             PropertyInfo[] objectProperties = objectToSerialize.GetType().GetProperties();
             ModelName = objectToSerialize.GetType().Name;
 
-            htmlObjects = new ObjectStruct[objectProperties.Length];
+            TypeStruct newStruct;
+            newStruct.htmlObjects = new ObjectStruct[objectProperties.Length];
 
             for (int i = 0; i < objectProperties.Length; ++i)
             {
-                htmlObjects[i].ObjectName = objectProperties[i].Name;
-
                 object t = objectProperties[i].GetValue(objectToSerialize, null);
                 if (t != null)
                 {
-                    htmlObjects[i].ObjectValue = t.ToString();
+                    newStruct.htmlObjects[i].ObjectValue = t.ToString();
                 }
                 else
                 {
-                    htmlObjects[i].ObjectValue = "NULL";
+                    newStruct.htmlObjects[i].ObjectValue = "NULL";
                 }
 
-                htmlObjects[i].ObjectType = objectProperties[i].PropertyType.FullName;
+                newStruct.htmlObjects[i].ObjectType = objectProperties[i].PropertyType.FullName;
             }
 
+            typeObjects.Add(newStruct);
+        }
+
+        private void GetHtmlTitles(object objectToSerialize)
+        {
+            PropertyInfo[] objectProperties = objectToSerialize.GetType().GetProperties();
+            htmlTitles = new string[objectProperties.Length];
+
+            for (int i = 0; i < objectProperties.Length; ++i)
+            {
+                htmlTitles[i] = objectProperties[i].Name;
+            }
         }
     }
 
