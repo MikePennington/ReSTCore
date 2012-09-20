a99fc2638521d22a1c13e809c72f7a0c5dbbf433
Mike Pennington <MikePennington@gamestop.com>
2012-09-20 13:57:48 -0500

    Allowing ability to specific a custom Uri for a RestDTO

--- ReSTCore/DTO/RestDTO.cs
+++ ReSTCore/DTO/RestDTO.cs
@@ -24,24 +24,7 @@ namespace ReSTCore.DTO
                 if (Id == null)
                     return;
 
-                if (Path != null)
-                {
-                    if (RestCore.Configuration != null && RestCore.Configuration.ServiceBaseUri != null)
-                    {
-                        Uri = RestCore.Configuration.ServiceBaseUri.Combine(Path).CombineUri(HttpUtility.UrlEncode(Id.ToString()));
-                    }
-                    else
-                    {
-                        var builder = HttpContext.Current == null
-                                          ? new UriBuilder()
-                                          : new UriBuilder(HttpContext.Current.Request.Url);
-                        builder.Scheme = "http";
-                        builder.Path = System.IO.Path.Combine(Path, HttpUtility.UrlEncode(Id.ToString()));
-                        if (builder.Port == 80)
-                            builder.Port = -1;
-                        Uri = builder.ToString();
-                    }
-                }
+                BuildUri();
             }
         }
 
@@ -51,5 +34,27 @@ namespace ReSTCore.DTO
         [XmlIgnore]
         [JsonIgnore]
         public abstract string Path { get; }
+
+        protected virtual void BuildUri()
+        {
+            if (Path != null)
+            {
+                if (RestCore.Configuration != null && RestCore.Configuration.ServiceBaseUri != null)
+                {
+                    Uri = RestCore.Configuration.ServiceBaseUri.Combine(Path).CombineUri(HttpUtility.UrlEncode(Id.ToString()));
+                }
+                else
+                {
+                    var builder = HttpContext.Current == null
+                                      ? new UriBuilder()
+                                      : new UriBuilder(HttpContext.Current.Request.Url);
+                    builder.Scheme = "http";
+                    builder.Path = System.IO.Path.Combine(Path, HttpUtility.UrlEncode(Id.ToString()));
+                    if (builder.Port == 80)
+                        builder.Port = -1;
+                    Uri = builder.ToString();
+                }
+            }
+        }
     }
 }
