8bd4cc7d8a4e05994b30114c45090474c15c56f5
Mike Pennington <MikePennington@gamestop.com>
2012-11-28 09:02:51 -0600

    Catching exception when Xsd cannot be created.

--- ReSTCore/Models/DtoModel.cs
+++ ReSTCore/Models/DtoModel.cs
@@ -26,25 +26,32 @@ namespace ReSTCore.Models
             IsValid = true;
             Name = dtoType.Name;
 
-            Xsd = ExtractXsdFromType(dtoType);
+            Xsd = ConstructXsdFromType(dtoType);
 
             var helpAttr = (HelpAttribute)Attribute.GetCustomAttribute(dtoType, typeof(HelpAttribute), false);
             if (helpAttr != null)
                 Description = helpAttr.Text;
         }
 
-        private string ExtractXsdFromType(Type type)
+        private string ConstructXsdFromType(Type type)
         {
-            XmlReflectionImporter importer = new XmlReflectionImporter();
-            XmlTypeMapping mapping = importer.ImportTypeMapping(type);
-            XmlSchemas xmlSchemas = new XmlSchemas();
-            XmlSchemaExporter xmlSchemaExporter = new XmlSchemaExporter(xmlSchemas);
-
-            using (var writer = new StringWriter())
+            try
             {
-                xmlSchemaExporter.ExportTypeMapping(mapping);
-                xmlSchemas[0].Write(writer);
-                return XElement.Parse(writer.ToString()).ToString();
+                XmlReflectionImporter importer = new XmlReflectionImporter();
+                XmlTypeMapping mapping = importer.ImportTypeMapping(type);
+                XmlSchemas xmlSchemas = new XmlSchemas();
+                XmlSchemaExporter xmlSchemaExporter = new XmlSchemaExporter(xmlSchemas);
+
+                using (var writer = new StringWriter())
+                {
+                    xmlSchemaExporter.ExportTypeMapping(mapping);
+                    xmlSchemas[0].Write(writer);
+                    return XElement.Parse(writer.ToString()).ToString();
+                }
+            }
+            catch (Exception e)
+            {
+                return e.Message;
             }
         }
     }
